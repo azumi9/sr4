8f40bd74d844b9d1f5d0128079970cc4a2738472
azumi9 <ekaterinakitajcenkova@gmail.com>
2024-11-12 18:43:43 +0000

    First version

--- (sr4.cpp
+++ (sr4.cpp
@@ -1,1 +1,12 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+	setlocale(0, ".1251");
+	double y, x; 
+	cout << "Введiть значення х= ";
+	cin >> x;
+	
+	return 0;
+}
